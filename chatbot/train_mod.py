0a5b39b33a0ace6a59afe9fe7c975e37d2ba8066
poojaravi01 <kuchan2001@gmail.com>
2021-09-16 11:03:06 +0530

    Create train_mod.py
    Added code for training the chatbot model.

---
+++ chatbot/train_mod.py
@@ -0,0 +1,67 @@
+from chatbot import preprocess_data,BotData,hyperparams
+import json
+from torch.utils.data import DataLoader,Dataset
+import torch.nn as nn
+import torch
+from torch.cuda.amp import autocast, GradScaler
+from model import BotModel, prune_func
+import time
+
+path=r'C:\Users\Pooja\Documents\.py_files\ausm_intents.json'
+with open(path,'rb') as f:
+    data=json.load(f,strict=False)
+xtrain, ytrain, words, labels = preprocess_data(data)
+input_size, output_size, num_layers, hidden_size, learning_rate = hyperparams(xtrain,ytrain)
+
+#set grad scaler instance
+scaler = GradScaler()
+
+#collect dataset
+df=BotData()
+train_loader=DataLoader(dataset=df,batch_size=8,shuffle=True)
+
+#load model
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+mod = BotModel(input_size, output_size, hidden_size).to(device)
+model = prune_func(mod)
+
+#set loss criterion and optimizer configurations
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+epochs = 100
+
+s = time.time()
+for epoch in range(epochs):
+    for (w,l) in train_loader:
+        w = w.reshape(8,1,137)
+        w = w.to(dtype=torch.float32).to('cuda:0')
+        l = l.to(dtype=torch.long).to('cuda:0')
+
+        optimizer.zero_grad()
+        with autocast():
+            outputs = model(w).to(device)
+            loss = criterion(outputs,l)
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+    if epoch % 10 == 0:
+        print('epoch: '+str(epoch)+', loss: '+str(loss.item()))
+
+e = time.time()
+print('Final loss: '+str(loss.item()))
+print('Training done')
+print('Time to train: ',(e-s))
+
+#save the model details
+model_data = {
+            'model_state': model.state_dict(),
+            'input_size': input_size,
+            'output_size': output_size,
+            'hidden_size': hidden_size,
+            'words': words,
+            'tags': labels
+}
+
+to_save = 'your_model.pth'
+torch.save(model_data, to_save)
